4d08630fbcc16028c88369fbc37e29046eee9377
Aswani kumari <kumariaswani4762@gmail.com>
2023-03-23 18:26:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Contacts from './Component/Contacts/Contacts'
+import AboutUs from './Component/AboutUs/AboutUs'
+import Header from './Component/Header/Header'
+import Home from './Component/Home/Home'
+import Profile from './Component/Profile/Profile'
+ import News from './Component/Body/News'
+import Footer from './Component/Footer/Footer'
+import Nav from './Component/Body/Nav'
+
+
+
 
 function App() {
+
+
+  // console.log(isAuthenticated)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    <BrowserRouter>
+      {
+         (
+          <div className='app'>
+            <Header />
+            <div className='app_body'>
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/profile' element={<Profile/>}/>
+                <Route path='/about-us' element={<AboutUs />} />
+                <Route path='/contacts' element={<Contacts/>} />
+
+                          
+    
+              </Routes>
+            </div>
+          
+           
+            
+            {/* 
+      
+        <div className='app_body'>
+          <AboutUs />
+        </div>
+
+        <Login /> */}
+
+          </div>
+        ) 
+      }
+      <div>
+        {/* <News/> */}
+     <News/>
+        
+      </div>
+      
+      <div>
+        <Footer/>
+      </div>
+    </BrowserRouter>
+    
+
+    
+
+  
+  )
 }
 
 export default App;
---
+++ src/Component/AboutUs/AboutUs.js
@@ -0,0 +1,17 @@
+import { Button } from '@mui/material'
+import React from 'react'
+import './AboutUs.css'
+
+function AboutUs() {
+    return (
+        <div className='about_us'>
+    
+
+            <Button variant='outlined'>
+                Go To Home Page
+            </Button>
+        </div>
+    )
+}
+
+export default AboutUs;
